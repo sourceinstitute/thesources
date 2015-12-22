bed3b7370fe475a262b7a3d87884562aebc01d51
Nemanja Cerovac <nemanja.cerovac@gmail.com>
2015-12-22 15:28:01 +0200

    major refactor of code and router change

--- server/perms.js
+++
@@ -1,7 +0,0 @@
-  Meteor.users.allow({
-    'update': function (userId,doc) {
-        //Todo: should check if the user is updating their own profile
-        //if (userId == Meteor.userId())
-          return true; 
-    }
-  });
